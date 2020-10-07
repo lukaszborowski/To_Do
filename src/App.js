7caaf699758dfb5a2b50c777732d6e2412ecd3ef
lukaszborowski <lukasz1borowski@gmail.com>
2020-10-07 15:26:36 +0200

    Still under develop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,84 @@ import './App.css';
 import "./styles.scss";
 
 
+// Dodawanie taska
+
+document.addEventListener('DOMContentLoaded', () => {
+    const todoList = document.querySelector('#todoList');
+    const todoForm = document.querySelector('#todoForm');
+    const todoSearch = document.querySelector('#todoSearch');
+    const todoTextarea = todoForm.querySelector('textarea');
+
+    function addTask(text) {
+      const element = React.cloneElement(Task);
+
+
+
+        //tworzę datę
+        const date = new Date();
+        const dateText = date.getFullYear();
+        document.querySelector(".task__date").innerText = dateText;
+
+        //wstawiam tekst
+        document.querySelector(".task__text").innerText = text;
+
+        //i wrzucam element do listy
+        todoList.append(element);
+    }
+
+    todoForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        if (todoTextarea.value !== '') {
+            addTask(todoTextarea.value);
+            todoTextarea.value = '';
+        }
+    });
+
+    todoList.addEventListener("click", e => {
+        if (e.target.classList.contains("task__delete")) {
+            e.target.closest(".task").remove();
+        }
+    });
+
+    todoSearch.addEventListener("input", () => {
+        const val = todoSearch.value;
+        const elems = todoList.querySelectorAll(".task");
+
+        for (const el of elems) {
+            const text = el.querySelector(".task__text").innerText;
+
+            if (text.includes(val)) {
+                el.style.setProperty("display", "");
+            } else {
+                el.style.setProperty("display", "none");
+            }
+        }
+    });
+});
+
+
+// task element
+
+class TaskElement extends Component {
+    render() {
+        return (
+            <>
+                <div className="element-bar">
+                    <h3 className="element-date"/>
+                    <button className="element-delete" title="Delete Task">
+                        Usuń
+                    </button>
+                </div>
+                <div className="element-text">
+                </div>
+            </>
+
+        )
+    }
+}
+
+
 // Header
 
 class Header extends Component {
@@ -63,6 +141,30 @@ class TaskList extends Component {
 }
 
 
+// Zadanie
+
+class Task extends Component {
+    render() {
+        return (
+            <div className="task">
+                <div className="task__bar">
+                    <h3 className="task__date">20-10-2016 godz. 16:32</h3>
+                    <button className="task__delete" title="Delete task">
+                        Usuń
+                    </button>
+                </div>
+                <div className="task__text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio laudantium quasi blanditiis
+                    enim molestias explicabo id totam veniam corporis maiores.
+                </div>
+            </div>
+        )
+
+
+    }
+}
+
+
 
 // Wyświetlanie
 
@@ -77,6 +179,7 @@ class Flex extends Component {
                 </div>
                 <div className="flex__container__right">
                     <TaskList />
+                    <Task />
                 </div>
             </div>
         )
