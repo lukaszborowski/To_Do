697d3fd900c2e68c7904a497e7c522c619e15889
lukaszborowski <lukasz1borowski@gmail.com>
2020-10-14 15:37:35 +0200

    Added remove from storage and task description

--- src/App.js
+++ src/App.js
@@ -24,13 +24,14 @@ class Form extends Component {
 
 
     state = {
-        task: "",
+        value: "",
         day: "",
         month: "",
         year: "",
         hour: "",
         minutes: "",
     };
+
 
     handleOnChange = (e) => {
         const date = new Date();
@@ -39,8 +40,10 @@ class Form extends Component {
             month: date.getMonth(),
             year: date.getFullYear(),
             hour: date.getHours(),
-            minutes: date.getMinutes()
-        };
+            minutes: date.getMinutes(),
+            value: e.target.value
+
+    };
 
         this.setState({
             day: datefull.day,
@@ -48,11 +51,14 @@ class Form extends Component {
             year: datefull.year,
             hour: datefull.hour,
             minutes: datefull.minutes,
-           // task: document.getElementsByTagName("textarea").innerText
+            value: datefull.value
+
         })
     };
 
+
     storageAdd = () => {
+
 
         if (localStorage.getItem("Tasks") === null || localStorage.getItem("Tasks") === "" || localStorage.getItem("Tasks") === undefined) {
             localStorage.setItem("Tasks", JSON.stringify([{
@@ -60,7 +66,8 @@ class Form extends Component {
                 month: this.state.month,
                 year: this.state.year,
                 hour: this.state.hour,
-                minutes: this.state.minutes
+                minutes: this.state.minutes,
+                value: this.state.value
             }]))
         } else {
 
@@ -71,27 +78,21 @@ class Form extends Component {
                 year: this.state.year,
                 hour: this.state.hour,
                 minutes: this.state.minutes,
-                // task: this.state.task
+                value: this.state.value
             });
 
             localStorage.setItem("Tasks", JSON.stringify(localStrObject));
         }};
 
-    storageRemove = () => {
-
-       // localStorage.removeItem("Tasks")
-
-
-    };
-
 
     render() {
         return (
             <section className="form__section">
-                <form className="form" id="todoForm">
+                <form  className="form" id="todoForm">
                     <div className="form__row">
-                        <label className="form__label" htmlFor="todoMessage">To Do</label>
-                        <textarea className="form__message" name="todoMessage" id="todoMessage" onChange={this.handleOnChange} placeholder="Write Your task" />
+                        <label className="form__label" htmlFor="todoMessage">To Do
+                        <textarea className="form__message" name="todoMessage" id="todoMessage" placeholder="Write Your Task" onChange={this.handleOnChange}/>
+                    </label>
                     </div>
                     <div className="form__row">
                         <button type="submit" className="button form__button" onClick={this.storageAdd}>Add to List</button>
@@ -128,26 +129,38 @@ class TaskList extends Component {
 
 // Zadania
 
-class Tasks extends Component {
-    render() {
-        return (
-            <>
-                <section className="task__list">
+class Task extends Component {
 
-                        {JSON.parse(localStorage.getItem("Tasks")).map((el,i) => {
-                            return <div key={i}> <strong>Added at:</strong> {el.day}.{el.month}.{el.year} <strong>Time:</strong> {el.hour}:{el.minutes}
+
+    storageRemove = () => {
+        let removeObj = JSON.parse(localStorage.getItem('Tasks'));
+        removeObj.splice(this.index, 1);
+        localStorage.setItem('Tasks', JSON.stringify(removeObj))
+    };
+
+
+
+        render()
+        {
+            return (
+                <>
+                    <section className="task__list">
+                        {JSON.parse(localStorage.getItem("Tasks")).map((el, i) => {
+                            return <div key={i}><strong>Added at:</strong> {el.day}.{el.month}.{el.year} &#32;
+                                 &nbsp; - {el.hour}:{el.minutes}
+                                <p><strong> Task description: </strong></p>
+                                {el.value}
                                 <button className="task__list__remove" onClick={this.storageRemove}>Remove</button>
                             </div>
 
                         })}
 
-                </section>
-            </>
-        )
-    }
+                    </section>
+                </>
+            )
+        }
+
 }
-
-
 
 // Wyświetlanie
 
@@ -162,7 +175,7 @@ class Flex extends Component {
                 </div>
                 <div className="flex__container__right">
                     <TaskList />
-                    <Tasks />
+                    <Task />
                 </div>
             </div>
         )
@@ -170,6 +183,7 @@ class Flex extends Component {
 }
 
 class App extends Component {
+
     render() {
         return (
            <Flex />
