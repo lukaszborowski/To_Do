9c066d3124cc4607a0e5b58be6ffaa567b749645
lukaszborowski <lukasz1borowski@gmail.com>
2020-10-19 16:29:11 +0200

    changed classes to hooks

--- src/App.js
+++ src/App.js
@@ -1,117 +1,120 @@
-import React, {Component} from "react";
+import React, {Component, useState } from "react";
 import './App.css';
 import "./styles.scss";
 
+
+ // Zegar
+ const Time = () => {
+
+//     const [state, setState] = useState({
+//         clock: new Date().toLocaleTimeString()
+//     });
+//
+//      const clockFunction = () => {
+//
+//          setState({
+//              clock: new Date().toLocaleTimeString()
+//          })
+//
+// }; setInterval(clockFunction, 1000);
+
+     return (
+         <div className="date">
+             <h1>
+                 <span>Today is:</span> { new Date().toDateString() }
+             </h1>
+         </div>
+     );
+
+ };
+
+
+
 // Header
 
-class Header extends Component {
-    render() {
-        return (
-            <section className="header__section">
-                <div className="header__container">
-                    <h1>Make a list of Your tasks</h1>
-                </div>
-            </section>
-
-        )
-    }
-}
-
+const Header = () => {
+    return (
+        <section className="header__section">
+            <div className="header__container">
+                <h2>Make a list of Your tasks</h2>
+            </div>
+        </section>
+    )
+};
 
 // Formularz
 
-class Form extends Component {
+const Form = () => {
+
+    const [state, setState] = useState({
+        value: "Sample Task text",
+        day: "Day",
+        time: "Hour:Minutes:Seconds",
+    });
 
 
-    state = {
 
-            value: "Sample Task text",
-            day: "Day",
-            month: "Month",
-            year: "Year",
-            time: "Hour:Minutes:Seconds",
-
-        };
-
-
-    handleOnChange = (e) => {
+    const handleOnChange = (e) => {
         const date = new Date();
         const datefull = {
-            day: date.getDate(),
-            month: date.getMonth(),
-            year: date.getFullYear(),
+            day: date.toDateString(),
             time: date.toLocaleTimeString(),
             value: e.target.value
+        };
 
-    };
-
-        this.setState({
+        setState({
             day: datefull.day,
-            month: datefull.month,
-            year: datefull.year,
             time: datefull.time,
             value: datefull.value
-
         })
     };
 
+        const storageAdd = (e) => {
+            if (!Boolean(localStorage.getItem("Tasks"))) {
+                localStorage.setItem("Tasks", JSON.stringify([{...state}]))
+            } else {
+                let localStrObject = JSON.parse(localStorage.getItem("Tasks"));
+                localStrObject.push({...state});
 
-    storageAdd = () => {
+                localStorage.setItem("Tasks", JSON.stringify(localStrObject));
+            }
+        };
 
-
-        if (localStorage.getItem("Tasks") === null || localStorage.getItem("Tasks") === "" || localStorage.getItem("Tasks") === undefined) {
-            localStorage.setItem("Tasks", JSON.stringify([{
-                day: this.state.day,
-                month: this.state.month,
-                year: this.state.year,
-                time: this.state.time,
-                value: this.state.value
-            }]))
-        } else {
-
-            let localStrObject = JSON.parse(localStorage.getItem("Tasks"));
-            localStrObject.push({
-                day: this.state.day,
-                month: this.state.month,
-                year: this.state.year,
-                time: this.state.time,
-                value: this.state.value
-            });
-
-            localStorage.setItem("Tasks", JSON.stringify(localStrObject));
-        }};
-
-
-    render() {
         return (
             <section className="form__section">
-                <form  className="form" id="todoForm">
+                <form className="form" id="todoForm" onSubmit={(e) => storageAdd(e)}>
                     <div className="form__row">
                         <label className="form__label" htmlFor="todoMessage">To Do
-                        <textarea className="form__message" name="todoMessage" id="todoMessage" placeholder="Write Your Task" onChange={this.handleOnChange}/>
-                    </label>
+                            <textarea
+                                className="form__message"
+                                name="todoMessage"
+                                id="todoMessage"
+                                placeholder="Write Your Task"
+                                onChange={(e) => handleOnChange(e)}
+                            />
+                        </label>
                     </div>
                     <div className="form__row">
-                        <button type="submit" className="button form__button" onClick={this.storageAdd}>Add to List</button>
+                        <button type="submit" className="button form__button">Add to List</button>
                     </div>
                 </form>
             </section>
         )
-    }
-}
+    };
+
 
 // Szukanie zadań - lista zadań
 
-class TaskList extends Component {
-    render() {
-        return (
+const TaskList = () => {
+    return (
+        <>
             <section className="list__section">
                 <header className="list__header">
                     <h2 className="list__title">
                         Task List:
                     </h2>
                     <form className="list__search__form">
-                        <input type="search" id="todoSearch" className="list__search" placeholder="Search Task"/>
+                        <input type="search" id="todoSearch" className="list__search" placeholder="Search Task" />
                     </form>
                 </header>
 
@@ -119,52 +122,58 @@ class TaskList extends Component {
 
                 </div>
             </section>
-        )
-    }
-}
 
+
+        </>
+    )
+};
 
 // Zadania
 
-class Task extends Component {
-    state = {
-        data: JSON.parse(localStorage.getItem('Tasks'))
-    };
-
-    storageRemove = (index) => {
+const Task = () => {
+    const [state, setState] = useState(JSON.parse(localStorage.getItem('Tasks')));
+    const storageRemove = (index) => {
         let removeObj = JSON.parse(localStorage.getItem('Tasks'));
         removeObj.splice(index, 1);
-        localStorage.setItem('Tasks', JSON.stringify(removeObj))
-        this.setState({
-            data: JSON.parse(localStorage.getItem("Tasks"))
-        })
+        localStorage.setItem('Tasks', JSON.stringify(removeObj));
+        setState(JSON.parse(localStorage.getItem("Tasks")))
     };
 
-    render() {
-        const tasks = this.state.data;
-        return (
-            <>
-                <section className="task__list">
-                    {Boolean(tasks) && tasks.map((el, i) => (
-                        <div key={i}><strong>Added at:</strong> <span> {el.day}.{el.month}.{el.year}  &#32;
-                            &nbsp; - {el.time} </span>
-                            <p> <strong className="task__list__description"> {el.value} </strong></p>
-                            <button className="task__list__remove" onClick={() => this.storageRemove(i)}>Remove</button>
-                        </div>
-                    ))}
-                </section>
-            </>
-        )
-    }
+    const removeAll = () => {
 
-}
+        const remove = prompt('Do You Really want remove all Tasks?', "No");
+        if (remove === 'Yes' || remove === 'yes' || remove === 'YES' || remove === 'YEs' || remove === 'yES' || remove === 'yeS') {
+            localStorage.clear();
+            setState(JSON.parse(localStorage.getItem("Tasks")));
+
+        }
+    };
+
+    return (
+        <>
+            <div className="remove__tasks">
+                <button onClick={() => removeAll()}>Clear All Tasks</button>
+            </div>
+            <section className="task__list">
+                {Boolean(state) && state.map((el, i) => (
+                    <div key={i}><strong>Added at:</strong> <span> {el.day}  &#32;
+                        &nbsp; - {el.time} </span>
+                        <p> <strong className="task__list__description"> {el.value} </strong></p>
+                        <button className="task__list__remove" onClick={() => storageRemove()}>Remove</button>
+                    </div>
+                ))}
+            </section>
+        </>
+    )
+};
+
 
 // Wyświetlanie
 
-
-class Flex extends Component {
-    render() {
-        return (
+const Flex = () => {
+    return (
+        <>
+            <Time />
             <div className="flex__container">
                 <div className="flex__container__left">
                     <Header />
@@ -175,19 +184,19 @@ class Flex extends Component {
                     <Task />
                 </div>
             </div>
-        )
-    }
-}
+        </>
+    )
+};
+
 
 class App extends Component {
 
     render() {
         return (
-           <Flex />
+            <Flex />
         )
     }
 
 }
 
 export default App;
-
